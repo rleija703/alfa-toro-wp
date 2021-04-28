0d9a58808fd9ae0a0296274f8ec1c9b2a0bbf3d2
Ruben Leija <ruben@rubens-mac-mini.myfiosgateway.com>
2021-04-28 12:46:50 -0400

    Style archive home list of posts

--- wp-content/themes/dontlookatmytheme/home.php
+++ wp-content/themes/dontlookatmytheme/home.php
@@ -4,36 +4,36 @@
         <h2>Join SEOs at Amazon, IBM & Cisco who get in-depth guides they can actually use</h2>
         <div class="subscribe-form"></div>
     </div>
-    <main>
-        <div class="blog-archive">
+    <main class="archive">
+        <div class="archive__posts">
             <?php if ( have_posts() ) : ?>
                 <?php while ( have_posts() ) : ?>
                     <?php the_post(); ?>
-                    <article>
-                        <header>
-                            <h2><?php the_title(); ?></h2>
-                            <div class="author">
+                    <article class="archive__post">
+                        <header class="archive__header">
+                            <h2 class="archive__title"><?php the_title(); ?></h2>
+                            <div class="archive__author">
                                 <?php print get_the_author_meta( "display_name" ); ?>
                             </div>
-                            <div class="post-date">
+                            <div class="archive__post-date">
                                 <?php print get_the_date( "F j, Y" ); ?>
                             </div>
                         </header>
                         <?php if ( has_post_thumbnail() ) : ?>
-                            <div>
+                            <div class="archive__img">
                                 <?php print get_the_post_thumbnail(); ?>
                             </div>
                         <?php endif; ?>
-                        <div class="post-excerpt">
+                        <div class="archive__excerpt">
                             <?php the_excerpt(); ?>
-                            <div class="read-more">
-                                <a href="<?php the_permalink(); ?>">View Post</a>
+                            <div class="archive__action">
+                                <a class="button" href="<?php the_permalink(); ?>">View Post</a>
                             </div>
                         </div>
                     </article>
                 <?php endwhile; ?>
             <?php endif; ?>
         </div>
-        <div class="widget-sidebar"></div>
+        <div class="archive__sidebar"></div>
     </main>
 <?php get_footer(); ?>
