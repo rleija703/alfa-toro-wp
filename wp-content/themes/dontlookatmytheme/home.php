9b13e72894ea72bd8c822f6a331ab445ce6ba72e
Ruben Leija <ruben@rubens-mac-mini.myfiosgateway.com>
2021-04-28 11:16:25 -0400

    add base typography styles

--- wp-content/themes/dontlookatmytheme/home.php
+++ wp-content/themes/dontlookatmytheme/home.php
@@ -5,6 +5,11 @@
         <div class="subscribe-form"></div>
     </div>
     <main>
+    <h1>Heading 1</h1>
+<h2>Heading 2</h2>
+<h3>Heading 3</h3>
+<h4>Heading 4</h4>
+<p>Paragraph text n;ajdalkjdklj</p>
         <div class="blog-archive">
             <?php if ( have_posts() ) : ?>
                 <?php while ( have_posts() ) : ?>
