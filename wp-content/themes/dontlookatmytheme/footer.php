a589f34fcdec760c1e82109ad3ff19d509adc8b1
Ruben Leija <ruben@rubens-mac-mini.myfiosgateway.com>
2021-04-29 10:59:12 -0400

    update footer & single post hero

--- wp-content/themes/dontlookatmytheme/footer.php
+++ wp-content/themes/dontlookatmytheme/footer.php
@@ -1,4 +1,20 @@
-        <div class="site-footer"></div>
+        <div class="site-footer">
+            <div class="site-footer__column">
+                <div class="site-footer__logo">
+                    Alfa Toro
+                </div>
+                <div class="site-footer__copyright">
+                    Copyright Alfa Toro, <?php print date( 'Y' ); ?>
+                </div>
+            </div>
+            <div class="site-footer__column">
+                <ul class="site-footer__menu">
+                    <li>
+                        <a href="<?php print site_url(); ?>">Home</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
         <?php wp_footer(); ?>
     </body>
 </html>
--- wp-content/themes/dontlookatmytheme/header.php
+++ wp-content/themes/dontlookatmytheme/header.php
@@ -4,6 +4,7 @@
         <meta charset="<?php bloginfo( 'charset' ); ?>">
         <meta name="viewport" content="width=device-width, initial-scale=1.0" >
 		<link rel="profile" href="https://gmpg.org/xfn/11">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700"/>
         <?php wp_head(); ?>
     </head>
     <body <?php body_class(); ?>>
